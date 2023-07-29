5126be10ff03e4c8932fdd72631e962789c5915a
bardium <114866826+bardium@users.noreply.github.com>
2023-07-29 17:39:17 -0400

    Update metadata.lua

--- metadata.lua
+++ metadata.lua
@@ -1,5 +1,5 @@
 return {
-	version = '1.02',
+	version = '1.03',
 	updated = '7/29/2023',
-	message = 'temp fix for damage type problems + auto equip wepaon'
+	message = 'better auto heal using Random().new'
 }
